79498f2d7d9f63eb22199ff78ccdd6051dbd2ccc
PilYeooong <pilyeooong@gmail.com>
2021-06-15 17:15:14 +0900

    feat. current user check logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  def current_user
+    return nil if request.headers['Authorization'].blank?
+    token = request.headers['Authorization'].split(' ').last
+    return nil if token.blank?
+    JwtToken.verify(token)
+  end
 end
